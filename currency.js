07a45343a801e4049b3db696f04f80b3efc8e03e
jaredgrady3 <79764653+jaredgrady3@users.noreply.github.com>
2021-11-14 10:31:42 -0600

    Add files via upload

---
+++ currency.js
@@ -0,0 +1,15 @@
+$.getJSON("http://api.currencylayer.com/live?access_key=75c5783545b9dede04d1a72f97cc2e62&format=1", 
+	function(data){
+		console.log(data);
+	
+});
+
+
+<script src="http://api.currencylayer.com/live"></script>
+
+//API KEY: 75c5783545b9dede04d1a72f97cc2e62
+// Constant URL value for JAAS API
+
+ ? access_key = 55cdd95ec40b07e6b5f3df2c73b641a3
+
+ & currencies = USD,AUD,CAD,PLN,MXN
